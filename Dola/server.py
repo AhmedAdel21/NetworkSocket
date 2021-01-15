9171ee2c4652a24da0ced476bcc6b557d0b55b3d
AhmedAdel21 <ahmed12365488@gmail.com>
2021-01-15 22:55:43 +0200

    without mongoDB

--- Dola/server.py
+++ Dola/server.py
@@ -25,9 +25,10 @@ sel.register(mySocket, selectors.EVENT_READ, data=None)
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
     clientsList.append(conn)
+    print(clientsList)
+    conn.send( bytes(str("connect to the Server: "),FORMAT) + bytes(str(addr[0]),FORMAT) + bytes(str(" at the Port: "),FORMAT) + bytes(str(addr[1]),FORMAT))
     print("accepted connection from", addr)
     conn.setblocking(False)
-    # sock.send(bytes(str("connect with the SERVER ["+str(HOST)+ "] at PORT ["+str(PORT))+"]",'utf-8'))
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
@@ -37,18 +38,24 @@ def service_connection(key, mask):
     data = key.data
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)  # Should be ready to read
-        print("we received " , recv_data)
+        print("we received " , recv_data.decode())
         if recv_data:
-            data.outb += recv_data
+            if recv_data.decode() == "!Disconn":
+                print("closing connection to", data.addr)
+                clientsList.remove(sock)
+                sel.unregister(sock)
+                sock.close()
+            else:
+                data.outb += recv_data
         else:
             print("closing connection to", data.addr)
+            clientsList.remove(sock)
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
         if data.outb:
-            print("sending message: ", repr(data.outb), ". to: ", data.addr)
             for client in clientsList:
-                client.send(data.outb)
+                client.send( bytes(str("User "),FORMAT) + bytes(str(key.fd),FORMAT) + bytes(str(": "),FORMAT) + data.outb)
             data.outb = b''
 
 while True:
@@ -60,43 +67,3 @@ while True:
             service_connection(key, mask)
 
 sel.close()
-
-"""
-def clients(c,addr):
-        print("connected with",addr)
-        c.send(bytes(str("connect with the SERVER ["+str(ip)+ "] at PORT ["+str(port))+"]",'utf-8'))
-        connected =True
-        while connected:
-            msg_lenght=c.recv(Header).decode(Format)
-            if msg_lenght:
-                msg_lenght=int(msg_lenght)
-                msg=c.recv(msg_lenght).decode(Format)
-                print(msg)
-                if msg == Disconn_msg:
-                    connected=False 
-                    c.send(bytes(str("disconnected with the SERVER ["+str(ip)+ "] at PORT ["+str(port)) +"]",Format))
-                    clients_dict.pop(addr[1])
-                    print(clients_dict)
-                else:
-                    response = chat_dict.get(msg)
-                    if response:
-                        c.send(bytes(str(response),Format))  
-                    else:
-                          c.send(bytes(str("What?!"),Format))  
-        print(f"connection with {addr} is closed ")
-        c.close()
-
-def start():
-    s.listen(3) # listening to three clients
-    print("waiting for connection")
-    while True:
-        c,addr =s.accept()
-        thread=threading.Thread(target=clients,args=(c,addr))
-        thread.start()
-        clients_dict.update({addr[1]:addr[0]})
-        print (clients_dict)
-        print(f"Active connection {threading.active_count()-1}")
-
-
-start()
-"""
