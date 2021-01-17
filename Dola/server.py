0738b743dd60f369000b6462133750987d322ce0
AhmedAdel21 <ahmed12365488@gmail.com>
2021-01-17 06:31:05 +0200

    final version

--- Dola/server.py
+++ Dola/server.py
@@ -8,11 +8,19 @@ import pymongo
 sel = selectors.DefaultSelector()
 
 ## connect to database and make the chatbot data
+
 myclient = pymongo.MongoClient("mongodb://localhost:27017/") #connect to MongoDB
+
 mydb = myclient["networkDB"] # create a database
-mydb.drop_collection("ChatBot")
-userData = mydb["ChatBot"] # create a collection 
-replies = open("replies.txt")
+
+mydb.drop_collection("ChatBot") # drop the previous data base
+
+userData = mydb["ChatBot"] # create a new collection 
+
+replies = open("replies.txt") #open th file that contain all replies
+
+## doing some processing to the data in file to be ready to insert it into our database
+
 repliesData = replies.read()
 data = []
 fisrt = repliesData.split("\n")
@@ -23,76 +31,108 @@ sendingData = []
 for i in data:
     sendingData.append({i[0]:' '.join(map(str, i[1:]))})
 
-userData.insert_many(sendingData)
+# inserting the data to our databese
+userData.insert_many(sendingData) 
 
 
 
-HOST = socket.gethostbyname(socket.gethostname())
-print("host : ",HOST)
-PORT = 9999
-Header = 64
-FORMAT = 'utf-8'
-DISCONN_MSG = "!Disconn"
+HOST = socket.gethostbyname(socket.gethostname())    # getting the Host name
+print("host : ",HOST) 
+
+PORT = 9999         # the port that we will run the server on
+
+FORMAT = 'utf-8'    # the formate we use to incode the message sent
+
+DISCONN_MSG = "!Disconn"    #disconnect message that will be sent from the client
+
 mySocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #create TCP socket ipv4
-mySocket.bind((HOST, PORT))
-mySocket.listen()
-clientsList=[]
-print("listening on", (HOST, PORT))
-mySocket.setblocking(False)
-# chat_dict = {"hi":"hi", "hello":"hello", "good morning":"good morning!","good night":"good night!","good evening":"good evening!"}
 
-sel.register(mySocket, selectors.EVENT_READ, data=None)
+mySocket.bind((HOST, PORT))
+
+mySocket.listen()
+
+clientsList=[]      # making a list to store all the conn objects
+
+print("listening on", (HOST, PORT))
+
+mySocket.setblocking(False)     # to make the server listen to multible conn without stucking in one of them
+
+sel.register(mySocket, selectors.EVENT_READ, data=None) 
 
 
 def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    clientsList.append(conn)
-    print(clientsList)
+
+    conn, addr = sock.accept()  # Accept the new connection 
+
+    clientsList.append(conn)    # take this conn socket object and store it to use it when we want to send any message to hime
+
+    # sending the connection message
     conn.send( bytes(str("connect to the Server: "),FORMAT) + bytes(str(addr[0]),FORMAT) + bytes(str(" at the Port: "),FORMAT) + bytes(str(addr[1]),FORMAT))
+
     print("accepted connection from", addr)
+
     conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"") # make an object to store the data in/out/addr of this connections/socket
+
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE   # making this socket doing read and write operation
+
+    sel.register(conn, events, data=data)   # resgister the socket to monitor it 
 
 def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
+
+    sock = key.fileobj # the socket object
+
+    data = key.data # the data object that we made for this socket
+
+    if mask & selectors.EVENT_READ: # if this is a reading operation go in it
+
+        recv_data = sock.recv(1024)  # read the message came from the client
+
         print("we received " , recv_data.decode())
-        if recv_data:
-            if recv_data.decode() == "!Disconn":
+        if recv_data: # if there any message go in
+
+            if recv_data.decode() == "!Disconn": # if this message if Disconn. So, close the socket
                 print("closing connection to", data.addr)
                 clientsList.remove(sock)
                 sel.unregister(sock)
                 sock.close()
             else:
+                #else store it in data object of this socket to resend it
                 data.outb += recv_data
         else:
+            # if there is no message close the connection
             print("closing connection to", data.addr)
             clientsList.remove(sock)
             sel.unregister(sock)
             sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            if len(clientsList) == 1:
+    if mask & selectors.EVENT_WRITE: # if it writing operation (we want to send message our clients) go in
+
+        if data.outb: # if there any data in our data object in the outb buffer go in
+            if len(clientsList) == 1: # if there is just one client in our list go in
+                # now the client will chat with our chatbot 
                 sock.send( bytes(str("User "),FORMAT) + bytes(str(key.fd),FORMAT) + bytes(str(": "),FORMAT) + data.outb)
                 for x in userData.find({},{ "_id": 0, data.outb.decode(): 1,}):
                     if x:
                         print(x[data.outb.decode()])
                         sock.send( bytes(str("\n"),FORMAT) +bytes(str("DoLaBot "),FORMAT) + bytes(str(": "),FORMAT) + bytes(str(x[data.outb.decode()]),FORMAT))
             else:
+                # so there are many clients connected to the server. So, there will chat with each other
+                # any message sent form any client will be resend to this clients and all other clients
                 for client in clientsList:
                     client.send( bytes(str("User "),FORMAT) + bytes(str(key.fd),FORMAT) + bytes(str(": "),FORMAT) + data.outb)       
             data.outb = b''
 
 while True:
+    # be there and waite the selectore to tell us if there is a new connection 
+    # or there is a connection want some services
     events = sel.select(timeout=None)
     for key, mask in events:
         if key.data is None:
+            # if it is none so this is a new connection. So, accept it
             accept_wrapper(key.fileobj)
         else:
+            # if it is not none so this is an accepted connection and the socket is established already 
             service_connection(key, mask)
 
 sel.close()
--- Dola/yahoo.py
+++ Dola/yahoo.py
@@ -16,19 +16,24 @@ class Yahoo(Ui_MainWindow):
     def __init__(self,mainwindow):
         super(Yahoo,self).setupUi(mainwindow)
         self.Message.returnPressed.connect(self.append_text)
-        self.send.clicked.connect(self.append_text)
-        self.connect.clicked.connect(self.connecting)
-        self.disconnect.clicked.connect(self.disconnecting)
-        self.isConnected=False
+        self.send.clicked.connect(self.append_text) # when th buttom clicked send the message to the server
+        self.connect.clicked.connect(self.connecting) # when the buttom conncet is pressed start the connection with the server
+        self.disconnect.clicked.connect(self.disconnecting) # when the buttom conncet is pressed close the connection with the server
+        self.isConnected=False # connection flag
+        
+        #making a timer to listen to the server
         self.timer = QtCore.QTimer()
         self.timer.setInterval(1000)
         self.timer.timeout.connect(self.listenToServer)
-        self.sel = selectors.DefaultSelector()
+        #server addr
         self.HOST = socket.gethostbyname(socket.gethostname())
         self.PORT = 9999
 
 
     def start_connections(self):
+        """
+        that our api to connect to the server 
+        """
         server_addr = (self.HOST, self.PORT)
         print('starting connection','to', server_addr)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,18 +43,23 @@ class Yahoo(Ui_MainWindow):
 
                 
     def connecting(self):
+        """
+        By calling this func the chat will connect with the server ...
+        """
         if not self.isConnected:
-            # self.isConnected=True
             self.start_connections()
-            # print("connected")
-            # self.server_text.append("connect with the SERVER ["+str(self.HOST)+ "] at PORT ["+str(self.PORT)+"]")
-            # self.state.setText("Connected")
             self.state.setStyleSheet("color: green")
             self.image.setPixmap(QtGui.QPixmap("conn.png")) 
 
     def disconnecting(self):
+        """
+        By calling this func the chat will close the connection with 
+
+        the server
+        """
         if self.isConnected:
             self.isConnected=False
+            self.timer.stop()
             self.sock.send(bytes("!Disconn",Format))
             self.server_text.append("disconnected with the SERVER ["+str(self.HOST)+ "] at PORT ["+str(self.PORT)+"]")
             self.state.setText("Disconnected")
@@ -57,6 +67,11 @@ class Yahoo(Ui_MainWindow):
             self.image.setPixmap(QtGui.QPixmap("disconn.png"))
 
     def listenToServer(self):
+        """
+        listen to the sever to know if there is any message is
+        
+        sent from any other chats or not
+        """
         if not self.isConnected:
             self.isConnected=True
             print("connected")
@@ -70,6 +85,9 @@ class Yahoo(Ui_MainWindow):
                 print("idle")
 
     def append_text(self):
+        """
+        this func send the new message to the server
+        """
         if self.isConnected:
             text=self.Message.text()
             self.sock.send(bytes(text,Format))
